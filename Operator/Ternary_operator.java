0bf06e95959523644c6dc23685c03ca3d2da0a91
Yogesh Kale <kaleyogesh252@gmail.com>
2026-08-20 14:39:35 +0530

    Control statment

---
+++ Operator/Ternary_operator.java
@@ -0,0 +1,8 @@
+public class Ternary_operator {
+    public static void main(String[] args){
+        int age = 20;
+        String result = (age >=18) ? "Adult" : "Not Adult";
+        System.out.println(result);
+    }
+    
+}
