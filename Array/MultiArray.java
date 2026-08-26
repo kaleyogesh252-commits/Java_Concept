4b7b88c9dbc3531382f1509c14c26d21e308c0ba
Yogesh Kale <kaleyogesh252@gmail.com>
2026-08-26 10:03:52 +0530

    Array And String EX

---
+++ Array/MultiArray.java
@@ -0,0 +1,17 @@
+public class MultiArray {
+    public static void main(String[] args) {
+        int [][] matrix = {
+            {10,20,30},
+            {40,50,60}
+        };
+        System.out.println(matrix.length);
+        for(int i= 0; i < matrix.length; i++){
+             for(int j= 0; j < matrix[i].length ; j++ ){
+                System.out.print(matrix[i][j] + " " );
+             }
+              System.out.println();
+        }
+       
+    }
+    
+}
